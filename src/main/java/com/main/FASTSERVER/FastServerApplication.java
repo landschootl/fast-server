c863447e151411febbb122ffaf8b2dc2c89d73c5
landschoot <tony.landschoot@outlook.com>
2019-04-03 11:31:35 +0200

    chore (springboot): add springboot in maven project

--- src/main/java/com/main/FASTSERVER/FastServerApplication.java
+++ src/main/java/com/main/FASTSERVER/FastServerApplication.java
@@ -1,11 +1,13 @@
 package com.main.FASTSERVER;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FastServerApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
-
+		SpringApplication.run(FastServerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/main/FASTSERVER/HelloController.java
@@ -0,0 +1,17 @@
+package com.main.FASTSERVER;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    public HelloController() {}
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
