a58904569de52ce5a37aa38489a53060a2c7123a
landscht <tony.landschoot@outlook.com>
2019-04-16 14:21:26 +0200

    refactor (processService): create new process for delete

--- src/main/java/com/main/fastserver/Scheduled/ScheduledTask.java
+++ src/main/java/com/main/fastserver/Scheduled/ScheduledTask.java
@@ -1,21 +1,12 @@
 package com.main.fastserver.Scheduled;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.main.fastserver.Entity.Domain;
 import com.main.fastserver.Entity.Skill;
-import com.main.fastserver.Entity.SubDomain;
-import com.main.fastserver.Service.DomainService;
-import com.main.fastserver.Service.SubDomainService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Component
