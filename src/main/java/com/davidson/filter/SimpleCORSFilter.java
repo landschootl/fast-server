7fc104541463eda7d0e49e5cc534f6bda697c988
landscht <tony.landschoot@outlook.com>
2019-04-29 11:20:54 +0200

    fix (cors): rename import package and delete commentary

--- src/main/java/com/davidson/filter/SimpleCORSFilter.java
+++ src/main/java/com/davidson/filter/SimpleCORSFilter.java
@@ -1,8 +1,7 @@
-package com.main.fastserver.filter;
+package com.davidson.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -12,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import java.io.IOException;
 
 @Component
@@ -23,15 +21,12 @@ public class SimpleCORSFilter extends GenericFilterBean {
      */
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Value("{URL_FRONT}")
-    private String allowOrigin;
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp,
                          FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        //String allowOrigin = System.getenv("URL_FRONT");
+        String allowOrigin = System.getenv("URL_FRONT");
 
         if(! StringUtils.isEmpty(allowOrigin) ) {
             response.setHeader("Access-Control-Allow-Origin", allowOrigin);
@@ -39,7 +34,6 @@ public class SimpleCORSFilter extends GenericFilterBean {
             response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-            //response.setHeader("Access-Control-Allow-Credentials", "true");
         }
 
         chain.doFilter(req, resp);
